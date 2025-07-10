ca771c17b8ba5f339fdf30fcd496e54b7442bd0a
Mashopy <eliasgheeraert@gmail.com>
2025-07-10 19:45:51 +0200

    connectivity: gen4m: Let scheduler handle cpu boosting
    Change-Id: Ibb68649d638e36b452274466b4d79c8c242eeecf

--- connectivity/wlan/core/gen4m/include/config.h
+++ connectivity/wlan/core/gen4m/include/config.h
@@ -2307,7 +2307,7 @@
 /* 1(default): Run on big core when tput over threshold
  * 0: Disable (Let system scheduler decide)
  */
-#define CFG_SUPPORT_TPUT_ON_BIG_CORE 1
+#define CFG_SUPPORT_TPUT_ON_BIG_CORE 0
 
 #define CFG_SUPPORT_LITTLE_CPU_BOOST 0
 
