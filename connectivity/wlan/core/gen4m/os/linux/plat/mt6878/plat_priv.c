0994230093f50e25052b6ca6cf8b3bb3e36054de
Mashopy <eliasgheeraert@gmail.com>
2025-07-10 20:46:00 +0200

    connectivity: gen4m: Disable CPU boosting from wlan by default
    Change-Id: Idb2434fc3a79db64f3f20a788fe174861d5bddcd

--- connectivity/wlan/core/gen4m/os/linux/plat/mt6878/plat_priv.c
+++ connectivity/wlan/core/gen4m/os/linux/plat/mt6878/plat_priv.c
@@ -79,6 +79,7 @@ int32_t kalCheckTputLoad(struct ADAPTER *prAdapter,
 			 int32_t i4Pending,
 			 uint32_t u4Used)
 {
+#if CFG_SUPPORT_CPU_BOOST
 	uint32_t pendingTh =
 		CFG_TX_STOP_NETIF_PER_QUEUE_THRESHOLD *
 		prAdapter->rWifiVar.u4PerfMonPendingTh / 100;
@@ -89,11 +90,15 @@ int32_t kalCheckTputLoad(struct ADAPTER *prAdapter,
 	       i4Pending >= pendingTh &&
 	       u4Used >= usedTh ?
 	       TRUE : FALSE;
+#else
+	return -1;
+#endif
 }
 
 #if KERNEL_VERSION(5, 4, 0) <= CFG80211_VERSION_CODE
 void kalSetTaskUtilMinPct(int pid, unsigned int min)
 {
+#if CFG_SUPPORT_CPU_BOOST
 	int ret = 0;
 	unsigned int blc_1024;
 	struct task_struct *p;
@@ -131,6 +136,7 @@ void kalSetTaskUtilMinPct(int pid, unsigned int min)
 		ret = sched_setattr(p, &attr);
 		put_task_struct(p);
 	}
+#endif
 }
 
 static LIST_HEAD(wlan_policy_list);
@@ -142,6 +148,7 @@ struct wlan_policy {
 
 void kalSetCpuFreq(int32_t freq, uint32_t set_mask)
 {
+#if CFG_SUPPORT_CPU_BOOST
 	int cpu, ret;
 	struct cpufreq_policy *policy;
 	struct wlan_policy *wReq;
@@ -183,6 +190,7 @@ void kalSetCpuFreq(int32_t freq, uint32_t set_mask)
 				wReq->cpu, freq, ret);
 		}
 	}
+#endif
 }
 
 void kalSetDramBoost(struct ADAPTER *prAdapter, u_int8_t onoff)
@@ -218,6 +226,7 @@ int32_t kalBoostCpu(struct ADAPTER *prAdapter,
 		    uint32_t u4TarPerfLevel,
 		    uint32_t u4BoostCpuTh)
 {
+#if CFG_SUPPORT_CPU_BOOST
 	struct GLUE_INFO *prGlueInfo = NULL;
 	static u_int8_t fgRequested = ENUM_CPU_BOOST_STATUS_INIT;
 	uint32_t u4CpuFreq = prAdapter->rWifiVar.au4CpuBoostMinFreq * 1000;
@@ -349,6 +358,9 @@ int32_t kalBoostCpu(struct ADAPTER *prAdapter,
 		"kalBoostCpu");
 
 	return 0;
+#else
+	return -1;
+#endif /* CFG_SUPPORT_CPU_BOOST */
 }
 #endif
 
@@ -407,6 +419,7 @@ void kalSetDrvEmiMpuProtection(phys_addr_t emiPhyBase, uint32_t offset,
 int32_t kalCheckVcoreBoost(struct ADAPTER *prAdapter,
 		uint8_t uBssIndex)
 {
+#if CFG_SUPPORT_CPU_BOOST
 #if (KERNEL_VERSION(5, 10, 0) <= CFG80211_VERSION_CODE) && \
 	(CFG_SUPPORT_802_11AX == 1)
 	struct BSS_INFO *prBssInfo;
@@ -454,4 +467,7 @@ int32_t kalCheckVcoreBoost(struct ADAPTER *prAdapter,
 #else
 	return FALSE;
 #endif
+#else /* CFG_SUPPORT_CPU_BOOST */
+	return FALSE;
+#endif /* CFG_SUPPORT_CPU_BOOST */
 }
