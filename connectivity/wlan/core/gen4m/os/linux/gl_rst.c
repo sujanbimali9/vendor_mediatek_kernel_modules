3ae5d8ad2085ace38eb5f21c3ea878dcb993008e
bengris32 <bengris32@protonmail.ch>
2026-01-09 05:34:31 +0530

    Galaga: connectivity: gen4m: Guard more wakelock code
    Signed-off-by: sm455 <221133194+sm455@users.noreply.github.com>

--- connectivity/wlan/core/gen4m/os/linux/gl_rst.c
+++ connectivity/wlan/core/gen4m/os/linux/gl_rst.c
@@ -58,7 +58,9 @@ u_int8_t g_isWaitRstOffComp;
 struct completion g_RstOffComp;
 struct completion g_RstOnComp;
 struct completion g_triggerComp;
+#if defined(CONFIG_ANDROID) && (CFG_ENABLE_WAKE_LOCK)
 KAL_WAKE_LOCK_T *g_IntrWakeLock;
+#endif
 struct task_struct *wlan_reset_thread;
 u_int8_t g_IsWholeChipRst = FALSE;
 u_int8_t g_SubsysRstCnt;
@@ -328,9 +330,11 @@ void glResetInit(struct GLUE_INFO *prGlueInfo)
 
 #if CFG_WMT_RESET_API_SUPPORT
 	init_completion(&wifi_rst.halt_comp);
+	#if defined(CONFIG_ANDROID) && (CFG_ENABLE_WAKE_LOCK)
 	KAL_WAKE_LOCK_INIT(NULL, g_IntrWakeLock, "WLAN_Reset");
 	KAL_WAKE_LOCK_INIT(NULL, wifi_rst.trigger_wakelock,
 			   "WLAN_Reset_Trigger");
+	#endif
 	init_waitqueue_head(&g_waitq_rst);
 	g_isWaitRstOffComp = FALSE;
 	init_completion(&g_RstOffComp);
@@ -377,8 +381,10 @@ void glResetUninit(void)
 	set_bit(GLUE_FLAG_HALT_BIT, &rst->ulFlag);
 	wake_up_interruptible(&g_waitq_rst);
 	wait_for_completion_interruptible(&rst->halt_comp);
+	#if defined(CONFIG_ANDROID) && (CFG_ENABLE_WAKE_LOCK)
 	KAL_WAKE_LOCK_DESTROY(NULL, wifi_rst.trigger_wakelock);
 	KAL_WAKE_LOCK_DESTROY(NULL, g_IntrWakeLock);
+	#endif
 #if IS_ENABLED(CFG_SUPPORT_CONNAC1X)
 	mtk_wcn_wmt_msgcb_unreg(WMTDRV_TYPE_WIFI);
 #endif
@@ -711,7 +717,9 @@ uint32_t glResetTrigger(struct ADAPTER *prAdapter,
 		u4RstFlag,
 		apucRstReason[eResetReason]);
 
+	#if defined(CONFIG_ANDROID) && (CFG_ENABLE_WAKE_LOCK)
 	KAL_WAKE_LOCK(NULL, rst->trigger_wakelock);
+	#endif
 
 	if (prAdapter) {
 		prChipInfo = prAdapter->chip_info;
@@ -793,8 +801,10 @@ exit:
 					 PCIE_VOTE_USER_LOG_RESET);
 #endif
 
+	#if defined(CONFIG_ANDROID) && (CFG_ENABLE_WAKE_LOCK)
 	if (KAL_WAKE_LOCK_ACTIVE(NULL, rst->trigger_wakelock))
 		KAL_WAKE_UNLOCK(NULL, rst->trigger_wakelock);
+		#endif
 
 	return rst_evt_send;
 }
@@ -2221,8 +2231,10 @@ int wlan_reset_thread_main(void *data)
 	       KAL_GET_CURRENT_THREAD_NAME(), KAL_GET_CURRENT_THREAD_ID());
 
 	while (TRUE) {
+		#if defined(CONFIG_ANDROID) && (CFG_ENABLE_WAKE_LOCK)
 		/* Unlock wakelock if hif_thread going to idle */
 		KAL_WAKE_UNLOCK(NULL, prWlanRstThreadWakeLock);
+		#endif
 		/*
 		 * sleep on waitqueue if no events occurred. Event contain
 		 * (1) GLUE_FLAG_HALT (2) GLUE_FLAG_RST
@@ -2342,7 +2354,9 @@ void glRstSetRstEndEvent(void)
 {
 	struct RESET_STRUCT *rst = &wifi_rst;
 
+	#if defined(CONFIG_ANDROID) && (CFG_ENABLE_WAKE_LOCK)
 	KAL_WAKE_LOCK(NULL, g_IntrWakeLock);
+	#endif
 
 	set_bit(GLUE_FLAG_RST_END_BIT, &rst->ulFlag);
 
